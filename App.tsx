e794199923b298b31d93f1d9b024fe731b57b530
matheus12344 <nationsworship5@gmail.com>
2025-03-12 13:16:40 -0300

    iniciando o container de histórico

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View, Dimensions, TextInput } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Dimensions, TextInput, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; // Import icons
 
 const { width, height } = Dimensions.get('window');
@@ -11,7 +11,7 @@ export default function App() {
   const [requestTime, setRequestTime] = useState('Agora');
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       {/*<---------------------Começo do Header ------------------------>*/}
       <StatusBar style="auto" />
       <View style={styles.header}>
@@ -38,23 +38,52 @@ export default function App() {
         </TouchableOpacity>
       </View>
       {/*<---------------------Fim do Header ------------------------>*/}
-    </View>
+
+      {/*<---------------------Começo do Histórico ------------------------>*/}
+
+      <TouchableOpacity style={styles.locationContainer}>
+        <View style={styles.locationIcon} />
+        <View>
+          <Text style={styles.locationTitle}>Joy tubos</Text>
+          <Text style={styles.locationAddress}>Rua Antonio de Siqueira, 267, Itaquaquecetuba-SP</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.locationContainer}>
+        <View style={styles.locationIcon} />
+        <View>
+          <Text style={styles.locationTitle}>Joy tubos</Text>
+          <Text style={styles.locationAddress}>Rua Antonio de Siqueira, 267, Itaquaquecetuba-SP</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.locationContainer}>
+        <View style={styles.locationIcon} />
+        <View>
+          <Text style={styles.locationTitle}>Joy tubos</Text>
+          <Text style={styles.locationAddress}>Rua Antonio de Siqueira, 267, Itaquaquecetuba-SP</Text>
+        </View>
+      </TouchableOpacity>
+      {/*<---------------------Fim do Histórico ------------------------>*/}
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: height * 0.05,
+    marginTop:-350
   },
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '80%',
-    position: 'absolute',
-    top: height * 0.1,
+    marginBottom: height * 0.05,
   },
   tabText: {
     fontSize: width * 0.08,
@@ -67,13 +96,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     width: '90%',
     height: height * 0.07,
-    position: 'absolute',
-    top: height * 0.15,
-    marginTop: height * 0.05,
     backgroundColor: 'lightgray',
     alignItems: 'center',
     borderRadius: 20,
     paddingHorizontal: width * 0.05,
+    marginBottom: height * 0.05,
   },
   icon: {
     marginRight: width * 0.02,
@@ -86,7 +113,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     padding: width * 0.02,
     borderRadius: 10,
-    marginLeft: 'auto',
     height: height * 0.05,
     alignItems: 'center',
     justifyContent: 'center',
@@ -94,5 +120,25 @@ const styles = StyleSheet.create({
   nowButtonText: {
     color: '#fff',
     fontSize: width * 0.03,
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: height * 0.02,
+    width: '90%',
+  },
+  locationIcon: {
+    backgroundColor: 'red',
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    marginRight: width * 0.05,
+  },
+  locationTitle: {
+    fontSize: width * 0.04,
+    fontWeight: 'bold',
+  },
+  locationAddress: {
+    fontSize: width * 0.035,
   },
 });
